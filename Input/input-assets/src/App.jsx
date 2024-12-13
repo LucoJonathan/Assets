97b6609c309dc2296dd8f9d23e332c6cf4aac5e2
Jonathan Luco <Jonathan.luco44@gmail.com>
2024-12-13 19:05:47 +0100

    feat: form style

--- Input/input-assets/src/App.jsx
+++ Input/input-assets/src/App.jsx
@@ -1,19 +1,45 @@
 import './App.css'
 
-function App() {
+function App () {
 
-  return (
-    <>
-
-            <label>
-                Input :
-                <input placeholder="test"/>
-            </label>
-            <button>Button</button>
+    return (<>
 
 
-    </>
-  )
+            <form id="register">
+                <div id="row">
+                    <label>
+                        Input :
+                        <input placeholder="Nom"/>
+                    </label>
+                    <label>
+                        Input :
+                        <input placeholder="Prenom"/>
+                    </label>
+                </div>
+                <div id="row">
+                    <label>
+                        Input :
+                        <input placeholder="Mail"/>
+                    </label>
+                    <label>
+                        Input :
+                        <input placeholder="Mail Confirm"/>
+                    </label>
+                </div>
+                <div id="row">
+                    <label>
+                        Input :
+                        <input placeholder="PassWord"/>
+                    </label> <label>
+                    Input :
+                    <input placeholder="pwd Confirm"/>
+                </label>
+                </div>
+                <div id="row">
+                    <button>Button</button>
+                </div>
+            </form>
+        </>)
 }
 
 export default App
